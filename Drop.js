40b6c06eb08cd6f81e0a31169d2c60587f2da4e8
Yatagaru <69464040+Yatagaru@users.noreply.github.com>
2022-01-10 22:31:46 -0300

    Update Drop.js

--- Drop.js
+++ Drop.js
@@ -1,26 +1,27 @@
 class Rain{
    constructor(x,y){
-    this.x = x
-    this.y = y
+    this.x = x;
+    this.y = y;
     var options ={
       friction: 0.1
     }
-    this.rain = Bodies.circle(x,y,5,options)
+    World.add(world, this.rain);
+    this.rain = Bodies.circle(x,y,5,options);
  
    }
 
 
 
 show(){
-  ellipseMode(RADIUS)
-   fill("blue")
-   ellipse(this.rain.position.x,this.rain.position.y,5,5)
+  ellipseMode(RADIUS);
+   fill("blue");
+   ellipse(this.rain.position.x,this.rain.position.y,5,5);
  
    }
 
 update(){
   if(this.rain.position.y>height){
-    Matter.Body.setPosition(this.rain, {x:random(0,400), y: random(0,400)})
+    Matter.Body.setPosition(this.rain, {x:random(0,400), y: random(0,400)});
   }
  }
 }
