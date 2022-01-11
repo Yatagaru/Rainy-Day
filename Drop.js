50d57fe8b5e226bb4596096eb435596f65e0f170
Yatagaru <69464040+Yatagaru@users.noreply.github.com>
2022-01-10 22:30:45 -0300

    Update Drop.js

--- Drop.js
+++ Drop.js
@@ -13,12 +13,13 @@ class Rain{
 
 show(){
   ellipseMode(RADIUS)
-  ellipse(this.rain.position.x,this.rain.position.y,5,5)
+   fill("blue")
+   ellipse(this.rain.position.x,this.rain.position.y,5,5)
  
    }
 
 update(){
-  if(frameCount%30===0){
+  if(this.rain.position.y>height){
     Matter.Body.setPosition(this.rain, {x:random(0,400), y: random(0,400)})
   }
  }
